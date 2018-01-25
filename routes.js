76b0e525278b81743f2deec93a8b686c342c637a
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-25 13:57:18 -0500

    Capri Template 1.0  /routes.js

---
+++ routes.js
@@ -0,0 +1,78 @@
+define([], function () {
+    return [
+        {
+            path: '/',
+            component: view('home')
+        },
+        {
+            path: '/stores',
+            component: view('default'),
+            children: [
+                {   
+                    path: '',
+                    component: view('stores'),
+                    name: 'storeList'
+                },
+                {
+                    path: ':id',
+                    component: view('store_details'),
+                    name: 'storeDetails',
+                    props: true
+                }
+            ]
+        },
+        {
+            path: '/events',
+            component: view('default'),
+            children: [
+                {
+                    path: '',
+                    component: view('events'),
+                    name: 'events'
+                },
+                {
+                    path: ':id',
+                    component: view('event_details'),
+                    name: 'eventDetails',
+                    props: true
+                }
+            ]
+        },
+    {
+      path: '/promotions',
+      component: view('default'),
+      children: [
+        {
+          path: '',
+          component: view('promotions'),
+          name: 'storeList'
+        },
+        {
+          path: ':id',
+          component: view('promotion_details'),
+          name: 'promotionDetails',
+          props: true
+        }
+      ]
+    },
+    {
+      path: '/404',
+      name: '404',
+      component: view('notfoundcomponent')
+    },
+    {
+      path: '*',
+      redirect: '/404'
+    }
+  ]
+
+  /**
+   * Asynchronously load view (lazy-loading)
+   * @param {string} name the filename (basename) of the view to load.
+   */
+  function view(name) {
+    return function(resolve) {
+      require(['vue!' + name + '.vue'], resolve);
+    }
+  };
+});
