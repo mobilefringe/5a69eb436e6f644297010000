6eb3803103b041af59b9818431ef220d4226a5b6
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-25 11:19:07 -0500

    Capri Template 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -1,45 +1,68 @@
 <template>
-  <div class="row">
-    <div class="large-6 columns">
-      <div>
-        <h1>{{title}}</h1>
-        <p>{{description}}</p>
-        <hr/>
-        <h5>Translated text example:</h5>
-        <p>{{ $t("message.hello") }}</p>
-        <p>{{property.name}}</p>
-        <hr/>
-        
-        
-        
-      </div>
+    <div><!-- without an outer container div this component template will not render -->
+        <np-loader v-if="!dataLoaded"></np-loader>
+        <transition name="fade">
+            <div class="home-banner-container" v-if="dataLoaded" v-cloak>
+                <!-- DYNAMIC BANNERS -->
+                <slick ref="slick" :options="slickOptions">
+                    <div v-for="banner in homeBanners" v-if="homeBanners">
+                        <div class="columns">
+                            <!--<router-link :to="banner.url" class="">-->
+                                <div class="home-banner" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
+                            <!--</router-link>-->
+                        </div>
+                    </div>
+                </slick>
+                <div class="hero-text" v-if="currentMessage">{{ currentMessage.title }}</div>
+            </div>
+        </transition>
     </div>
-  </div>
 </template>
-
 <script>
-  define(["Vue", "vuex", "vue!today_hours.vue", "vue!search-component.vue"], function(Vue, Vuex, TodayHoursComponent, SearchComponent) {
-    return Vue.component("home-component", {
-      template: template, // the variable template will be injected
-      data: function() {
-        return {
-          title: "MM with Vue.js!",
-          description: "An example of integration of Mall Maverick with Vue.js",
-          suggestionAttribute: 'name',
-          search: ''
-        }
-      },
-      computed: {
-        ...Vuex.mapGetters([
-          'property',
-          'processedStores'
-        ])
-      },
-      methods: {
-        onOptionSelect(option) {
-          console.log('Selected option:', option)
-        }
-      }
+    define(["Vue", "vuex", "vue-meta", "vue!vue-slick"], function(Vue, Vuex, Meta, slick){
+        Vue.use(Meta);
+        return Vue.component("home-component", {
+            template: template, // the variable template will be injected
+            data: function() {
+                return {
+                    dataLoaded: false,
+                    
+                    slickOptions: {
+                        arrows: false,
+                        autoplay: true,
+                        autoplaySpeed: 6000,
+                        cssEase: 'linear',
+                        dots: false,
+                        fade: true,
+                        infinite: true,
+                        slidesToShow: 1,
+                        speed: 1600
+                    }
+                }
+            },
+            created(){
+                this.loadData().then(response => {
+                    this.dataLoaded = true;      
+                });
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property'
+                ]),
+                homeBanners() {
+                    return _.orderBy(this.$store.state.banners, ['position'], ['asc']);
+                }
+            },
+            methods: {
+                loadData: async function() {
+                    try {
+                        let results = await Promise.all([this.$store.dispatch("getData", "banners"), this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/api/v3/northpark/messages.json"})]);
+                        return results;
+                    } catch(e) {
+                        console.log("Error loading data: " + e.message);    
+                    }
+                }
+            }
+        })
     })
-  })
 </script>
