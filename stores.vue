f8821c6f0d96cae47c8f68353306bffcbabab495
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-26 12:18:25 -0500

    Capri Template 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -24,7 +24,9 @@
                 <div class="row">
                     <div class="col-md-12" v-for="(stores, index) in storesByAlphaIndex" v-if="listMode === 'alphabetical'">
                         <div class="list_header">
-                            <span class="store_initial"><span>{{index}}</span></span>
+                            <div class="store_initial">
+                                <span>{{index}}</span>
+                            </div>
                         </div>
                         <div class="store-section" v-for="store in stores">
                             <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">
