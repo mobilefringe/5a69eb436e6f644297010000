5f03f3b86b5d5638aeaeee5a4efd39cf628e48c1
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-26 11:55:43 -0500

    Capri Template 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -132,48 +132,6 @@
                             </li>
                         </ul>
                     </nav>
-                    <!--<div class="main_menu">-->
-                    <!--    <ul class="main_nav">-->
-                    <!--        <li class="dropdown" v-on:mouseover="mouseOver" v-bind:class="{ open: active }">-->
-                                <!--<span class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">-->
-                    <!--                Shopping-->
-                                <!--</span>-->
-                    <!--            <ul class="dropdown-menu">-->
-                    <!--                <router-link tag="li" to="/stores" active-class="active" exact>-->
-                    <!--                    <a>Stores</a>-->
-                    <!--                </router-link>-->
-                    <!--                <router-link tag="li" to="/hours" active-class="active" exact>-->
-                    <!--                    <a>Hours</a>-->
-                    <!--                </router-link>-->
-                    <!--            </ul>-->
-                    <!--        </li>-->
-                    <!--        <li class="dropdown">-->
-                    <!--            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">-->
-                    <!--                Events-->
-                    <!--            </a>-->
-                    <!--            <ul class="dropdown-menu">-->
-                    <!--                <li><a href="/events">All Events</a></li>-->
-                    <!--                <li><a href="/pages/ptc-in-the-community">In the <br />Community</a></li>-->
-                    <!--            </ul>-->
-                    <!--        </li>-->
-                            <!--<li class="dropdown"><a href="/promotions">Promotions</a></li>-->
-                    <!--        <router-link tag="li" to="/promotions" class="dropdown" active-class="active" exact>-->
-                    <!--            <a>Promotions</a>-->
-                    <!--        </router-link>-->
-                    <!--        <li class="dropdown">-->
-                    <!--            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">-->
-                    <!--                Guest Services-->
-                    <!--            </a>-->
-                    <!--            <ul class="dropdown-menu">-->
-                    <!--                <li><a href="/pages/ptc-accessibility">Accessibilty</a></li>-->
-                    <!--                <li><a href="/pages/ptc-mall-services">Mall Services</a></li>-->
-                    <!--                <li><a href="/pages/ptc-gift-cards">Gift Cards</a></li>-->
-                    <!--                <li><a href="/pages/ptc-wifi">WiFi</a></li>-->
-                    <!--            </ul>-->
-                    <!--        </li>-->
-                    <!--        <li class="dropdown"><a href="/pages/ptc-coming-soon"  >Blog</a></li>-->
-                    <!--    </ul>-->
-                    <!--</div>-->
                 </div>
             </div>
         </section>
@@ -214,19 +172,8 @@
                 ])
             },
             methods: {
-                mouseOver: function() {
-                    this.active = !this.active;
-                    console.log("flag " + this.active);
-                }
+                
             }
-            // methods: {
-            //     onOptionSelect(option) {
-            //         console.log('Selected option:', option);
-            //         this.$router.push("/stores/" + option.slug);
-            //         this.show_search = false;
-            //         this.search = '';
-            //     }
-            // }
         });
     });
 </script>
