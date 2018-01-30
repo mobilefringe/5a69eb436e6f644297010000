7639b7507b48f27e85fd239418e782833fa0e17b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-30 14:51:56 -0500

    Capri Template 1.0  /messages.vue

---
+++ messages.vue
@@ -0,0 +1,27 @@
+<template>
+    
+</template>
+
+<script>
+    define(["Vue", "vuex", "vue_router", "routes"], function (Vue, Vuex, VueRouter, appRoutes) {
+        return Vue.component("messages-component", {
+            template: template, // the variable template will be injected,
+            data: function () {
+                return {
+                    
+                }
+            },
+            
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'processedStores',
+                    'route'
+                ])
+            },
+            methods: {
+                
+            }
+        });
+    });
+</script>
