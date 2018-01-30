8e0adfc61eb1337070839484af656618fbb3e59b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-30 14:39:03 -0500

    Capri Template 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -1,7 +1,9 @@
 <template>
     <div> <!-- without an outer container div this component template will not render -->
         <loading-spinner v-if="!dataLoaded"></loading-spinner>
-        <inside-header-component></inside-header-component>
+        <transition name="fade">
+            <inside-header-component></inside-header-component>
+        </transition>
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak class="main_container margin_30">
                 <div class="events_container" v-for="event in events">
