33df2ea3a2130beaf08fdcd34913d4a6d8f7152d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-30 14:54:57 -0500

    Capri Template 1.0  /messages.vue

--- messages.vue
+++ messages.vue
@@ -6,21 +6,31 @@
     define(["Vue", "vuex", "vue_router", "routes"], function (Vue, Vuex, VueRouter, appRoutes) {
         return Vue.component("messages-component", {
             template: template, // the variable template will be injected,
-            data: function () {
+            data: function() {
                 return {
-                    
+                    currentMessage: null
                 }
             },
-            
+            created(){
+                this.loadData().then(response => {
+                    this.currentMessage = response[1].data.messages.welcome[0].messages[0];
+                    this.dataLoaded = true;      
+                });
+            },
             computed: {
                 ...Vuex.mapGetters([
-                    'property',
-                    'processedStores',
-                    'route'
-                ])
+                    'property'
+                ]),
             },
             methods: {
-                
+                loadData: async function() {
+                    try {
+                        let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', { url:this.property.mm_host + "/api/v3/northpark/messages.json"})]);
+                        return results;
+                    } catch(e) {
+                        console.log("Error loading data: " + e.message);    
+                    }
+                }
             }
         });
     });
