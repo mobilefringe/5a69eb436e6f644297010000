842d329d42c0a74c9e1362b69188428fe7188e6f
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-01-30 12:09:20 -0500

    Capri Template 1.0  /inside_header.vue

--- inside_header.vue
+++ inside_header.vue
@@ -8,36 +8,22 @@
 
 <script>
     define(["Vue", "vuex"], function (Vue, Vuex) {
-        return Vue.component("header-component", {
+        return Vue.component("inside-header-component", {
             template: template, // the variable template will be injected,
             data: function () {
                 return {
                     active: false,    
                 }
             },
-            props:['social_media'],
             watch: {
-                show_menu: function() {
-                    if(this.show_menu == true){
-                        document.body.classList.add("no-scroll");
-                    } else if (this.show_menu == false) {
-                        document.body.classList.remove("no-scroll");
-                    }
-                },
-                show_search: function(){
-                    if(this.show_search == true){
-                        document.body.classList.add("no-scroll");
-                    } else if (this.show_search == false) {
-                        document.body.classList.remove("no-scroll");
-                    } 
-                }
+                
             },
             computed: {
                 ...Vuex.mapGetters([
                     'property',
-                    'processedStores',
-                    'route'
-                ])
+                    'timezone',
+                    'getPropertyHours'
+                ]),
             },
             methods: {
                 
