baa85b68f4fff1e0e15812ddb2b442e425716a45
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-02-02 14:40:19 -0500

    Capri Template 1.0  /job_details.vue

--- job_details.vue
+++ job_details.vue
@@ -19,8 +19,8 @@
                                     {{ currentJob.start_date | moment("MMM D, YYYY", timezone)}} to {{ currentJob.end_date | moment("MMM D, YYYY", timezone)}}
                                 </p>
                                 <p class="promo_date" v-else>{{ currentJob.start_date | moment("ddd, MMM D, YYYY", timezone)}}</p>
-                                <p class="dates">Telephone:<br />{{contact_phone}}</p>
-                                <p class="dates">{{message}}</p>
+                                <p v-if="currentJob.contact_phone" class="dates">Telephone:<br />{{ currentJob.contact_phone }}</p>
+                                <p v-if="currentJob.message" class="dates">{{ currentJob.message }}</p>
                             </div>
                             <div v-html="currentJob.description"></div>
                             <div class="row"> 
