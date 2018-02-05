0ae8c257d16345704ede836cd1d1959e5a388612
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-02-05 11:53:03 -0500

    Capri Template 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -79,6 +79,10 @@
                     <img src="//codecloud.cdn.speedyrails.net/sites/579231876e6f6467cf000000/image/png/1470851965000/menu.png" class="open_menu" alt="open menu">
                 </div>
                 <div class="col-md-9 hidden_phone">
+                    <div class="top-bar-right">
+                        <a v-on:click="changeLocale('fr-ca')" v-if="locale === 'en-ca'">FR</a>
+                        <a v-on:click="changeLocale('en-ca')" v-if="locale === 'fr-ca'">EN</a>
+                    </div>
                     <div class="header_social">
                         <div class="social_icons">
                             <span v-for="item in social_media">
