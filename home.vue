17b3a0c46e660d86d1a7730656c90e0484d602e7
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-02-05 12:20:22 -0500

    Capri Template 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -4,7 +4,7 @@
         <transition name="fade">
             <div class="home-banner-container" v-if="dataLoaded" v-cloak>
                 <slick ref="slick" :options="slickOptions">
-                    <div v-for="banner in homeBanners" v-if="homeBanners">
+                    <div v-if="homeBanners" v-for="banner in homeBanners">
                         <!--<router-link :to="banner.url" class="">-->
                             <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
                         <!--</router-link>-->
