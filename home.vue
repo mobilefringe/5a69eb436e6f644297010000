c012ea4a6465746cc6a1a20dc08e5afbae9f1271
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-02-05 12:30:00 -0500

    Capri Template 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -68,7 +68,7 @@
             methods: {
                 loadData: async function() {
                     try {
-                        let results = await Promise.all([this.$store.dispatch("getData", "banners")]);
+                        let results = await Promise.all([this.$store.dispatch("getData", "banners"), this.$store.dispatch("getData", "feature_items")]);
                         return results;
                     } catch(e) {
                         console.log("Error loading data: " + e.message);    
