e3c1ae6f572bc7a6d8ec6d73cda398b4a3c7f292
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-02-06 09:41:18 -0500

    Capri Template 1.0  /photos.vue

--- photos.vue
+++ photos.vue
@@ -7,7 +7,7 @@
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak class="main_container margin_30">
                 <div class="row">
-                    <div v-for="image in images" class="col-md-6">
+                    <div v-for="image in images" class="col-md-6 photo-container">
                         <a :href="image.image_url" :data-lightbox="image.name">
                             <img v-lazy="image.image_url" :alt="image.name"/>
                         </a>
