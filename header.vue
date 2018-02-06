0a5160291046f3e455b814106a2094e56c47f91b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-02-06 14:33:00 -0500

    Capri Template 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -4,7 +4,7 @@
             <div class="row logo_container">
                 <div class="col-md-2">
                     <a href="/">
-                        <img alt="Property Logo" class="site_logo" src="//codecloud.cdn.speedyrails.net/sites/579231876e6f6467cf000000/image/png/1469552090000/PTC-Logo-x2.png">
+                        <img alt="Property Logo" class="site_logo" src="//codecloud.cdn.speedyrails.net/sites/5a69eb436e6f644297010000/image/png/1517945546000/Group 4@2x.png">
                     </a>
                     <!--<img src="//codecloud.cdn.speedyrails.net/sites/579231876e6f6467cf000000/image/png/1470851965000/menu.png" class="open_menu" alt="open menu">-->
                 </div>
