c5addd65f03040b7d14e579766070c9b8d82b584
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-02-08 10:35:20 -0500

    Capri Template 1.0  /messages.vue

--- messages.vue
+++ messages.vue
@@ -28,7 +28,7 @@
             methods: {
                 loadData: async function() {
                     try {
-                        let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/api/v3/northpark/messages.json"})]);
+                        let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/api/v3/twinpines/messages.json"})]);
                         return results;
                     } catch(e) {
                         console.log("Error loading data: " + e.message);    
