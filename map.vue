9913e321772edec1d13b8b9e6b4afdbef4cf86ec
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-02-08 12:23:38 -0500

    Capri Template 1.0  /map.vue

---
+++ map.vue
@@ -0,0 +1,18 @@
+<template>
+    <div> <!-- without an outer container div this component template will not render -->
+        <loading-spinner v-if="!dataLoaded"></loading-spinner>
+        <transition name="fade">
+            <inside-header-component></inside-header-component>
+        </transition>
+        <transition name="fade">
+            <div v-if="dataLoaded" v-cloak class="main_container margin_30">
+                <div class="row">
+
+
+                    <svg-map v-bind:svg-map-url="getSVGurl" :zoom="true" :pan="true" :zoomLimit="[0,10]" :regions="regions"></svg-map>
+                    
+                </div>
+            </div>
+        </transition>
+    </div>
+</template>
