12e52c98e399d560da86b7debe3cd2872d57958d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-02-09 12:25:31 -0500

    Capri Template 1.0  /loading.vue

--- loading.vue
+++ loading.vue
@@ -13,29 +13,29 @@
             template: template, // the variable template will be injected,
             data: function () {
                 return {
-                    dataLoaded: false
+                    // dataLoaded: false
                 };
             },
-            created (){
-                this.loadData().then(response => {
-                    this.dataLoaded = true;
-                });
-            },
+            // created (){
+            //     this.loadData().then(response => {
+            //         this.dataLoaded = true;
+            //     });
+            // },
             computed: {
                 ...Vuex.mapGetters([
                     'property'
                 ])
             },
-            methods: {
-                loadData: async function () {
-                    try {
-                        let results = await Promise.all([this.$store.dispatch("getData", "property")]);
-                        return results;
-                    } catch (e) {
-                        console.log("Error loading data: " + e.message);
-                    }
-                }
-            }
+            // methods: {
+            //     loadData: async function () {
+            //         try {
+            //             let results = await Promise.all([this.$store.dispatch("getData", "property")]);
+            //             return results;
+            //         } catch (e) {
+            //             console.log("Error loading data: " + e.message);
+            //         }
+            //     }
+            // }
         });
     });
 </script>
