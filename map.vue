ecb1a51b474068447cfc16cd90bc5fc4fca71232
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-02-09 15:38:26 -0500

    Capri Template 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -80,7 +80,7 @@
                     this.svgMapRef.hideMarkers();
                     this.svgMapRef.addMarker(store, '//codecloud.cdn.speedyrails.net/sites/589e308f6e6f641b9f010000/image/png/1484850466000/show_pin.png');
                     this.svgMapRef.setViewBox(store)
-                },
+                }
             }
         });
     });
