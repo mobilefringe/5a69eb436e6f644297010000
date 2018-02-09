e832de351c5db18108621641f6c5d88581f8ece0
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-02-08 21:50:19 -0500

    Capri Template 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -39,8 +39,7 @@
             },
             computed: {
                 ...Vuex.mapGetters([
-                    'property',
-                    'processedStores'
+                    'property'
                 ]),
                 allStores() {
                     return this.processedStores;
@@ -76,7 +75,6 @@
                 },
                 dropPin(store) {
                     this.svgMapRef.hideMarkers();
-                    console.log(store);
                     this.svgMapRef.addMarker(store, '//codecloud.cdn.speedyrails.net/sites/589e308f6e6f641b9f010000/image/png/1484850466000/show_pin.png');
                     this.svgMapRef.setViewBox(store)
                 },
