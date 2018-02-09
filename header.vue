f1af8e77fe33f896c95c26b227565cf31f3e7ebe
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-02-09 10:19:11 -0500

    Capri Template 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -4,7 +4,7 @@
             <div class="row logo_container">
                 <div class="col-md-2">
                     <a href="/">
-                        <img alt="Property Logo" class="site_logo" src="//codecloud.cdn.speedyrails.net/sites/5a69eb436e6f644297010000/image/png/1517945546000/Group 4@2x.png">
+                        <img alt="Property Logo" class="site_logo center-block" src="//codecloud.cdn.speedyrails.net/sites/5a69eb436e6f644297010000/image/png/1517945546000/Group 4@2x.png">
                     </a>
                     <div @click="show_menu = !show_menu" :class="{ open: show_menu }" id="menu-icon">
                         <span></span>
