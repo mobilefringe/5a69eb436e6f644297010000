7511f5a78cdbb3b87c746ec7889196bace97177b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-02-11 17:21:46 -0500

    Capri Template 1.0  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -39,7 +39,7 @@
                                     </label>
                                 </div>
     					    </div>
-    					    <br/>
+    					    <div class="margin_20"></div>
                             <div class="col-xs-12">
                                 <button class="animated_btn" type="submit" :disabled="formSuccess">Subscribe</button>
                             </div>
