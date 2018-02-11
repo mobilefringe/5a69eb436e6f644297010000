9025c80be983594c5601fed6e8550fbd4dc5fc0a
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-02-11 17:24:13 -0500

    Capri Template 1.0  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -51,86 +51,51 @@
     </div>
 </template>
 <script>
-    define(["Vue", "vuex", "vue-meta", "jquery", "vee-validate"], function (Vue, Vuex, Meta, $, VeeValidate) {
+    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "vue-meta", 'vee-validate', 'jquery'], function(Vue, Vuex, moment, tz, VueMoment, Meta, VeeValidate, $) {
         Vue.use(Meta);
         Vue.use(VeeValidate);
         return Vue.component("newsletter-component", {
             template: template, // the variable template will be injected
-            data: function data() {
+            data: function() {
                 return {
-                    dataLoaded: false,
-                    form_data: {},
-                    loginPending: null,
-                    formSuccess: false,
-                    formError: false,
-                    time: new Date()
+                    currentPage: null,
+                    form_data : {},
+                    formSuccess : false,
+                    formError: false
                 }
             },
             mounted () {
                 this.form_data.email = this.$route.query.email;
-                console.log(this.form_data.email)
                 $("#newsletter_email").val(this.form_data.email);
             },
             watch : {
                 $route () {
                     this.form_data.email = this.$route.query.email;
-                    console.log(this.form_data.email)
                     $("#newsletter_email").val(this.form_data.email);
                 }
             },
-            created() {
-                this.loadData().then(response => {
-                    this.dataLoaded = true;
-                });
-            },
             computed: {
                 ...Vuex.mapGetters([
-                    'property'
+                    'property',
+                    'timezone',
                 ])
             },
             methods: {
-                loadData: async function () {
-                    try {
-                        let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "/pages/milton-contact-us.json"})]);
-                        return results;
-                    } catch (e) {
-                        console.log("Error loading data: " + e.message);
-                    }
-                },
-                validateBeforeSubmit() {
+                validateBeforeSubmit(form) {
                     this.$validator.validateAll().then((result) => {
                         if (result) {
                             let errors = this.errors;
-                            send_data = {};
-                            send_data.form_data = JSON.stringify(this.serializeObject(this.form_data));
-                            this.$store.dispatch("CONTACT_US", send_data).then(res => {
-                                this.formSuccess = true;
-                            }).catch(error => {
-                                try {
-                                    if (error.response.status == 401) {
-                                        console.log("Data load error: " + error.message);
-                                        this.formError = true;
-                                    } else {
-                                        console.log("Data load error: " + error.message);
-                                        this.formError = true;
-                                    }
-                                } catch (e) {
-                                    console.log("Data load error: " + error.message);
-                                    this.formError = true;
-                                }
-                            })
+                            
+                            if(errors.length > 0) {
+                                console.log("Error");
+                            }
+                            else {
+                                console.log("No Error");
+                                // return true;
+                                form.target.submit();
+                            }
                         }
                     })
-                },
-                serializeObject(obj) {
-                    var newObj = [];
-                    _.forEach(obj, function (value, key) {
-                        var tempVal = {};
-                        tempVal.name = key;
-                        tempVal.value = value;
-                        newObj.push(tempVal);
-                    });
-                    return newObj;
                 }
             }
         });
