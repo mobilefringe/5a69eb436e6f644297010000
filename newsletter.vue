a2a90c57d397546990388584737766251d906ccf
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-02-11 17:12:41 -0500

    Capri Template 1.0  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -22,17 +22,14 @@
                     </div>
                     <div class="col-md-6">
                         <form class="form-horizontal" action="//mobilefringe.createsend.com/t/d/s/irudui/" method="post" @submit.prevent="validateBeforeSubmit">
-                            <div class="form-group ">
-                                <div class="col-sm-6 col-xs-12" >
-                                    <label for="cm-name">Name</label>
-                                    <input v-model="form_data.name" required class="form-control" name="cm-name" type="text" placeholder="Name">
-                                </div>
+
+                            <div class="col-xs-12" >
+                                <label for="cm-name">Name</label>
+                                <input v-model="form_data.name" required class="form-control" name="cm-name" type="text" placeholder="Name">
                             </div>
-                            <div class="form-group">
-                                <div class="col-sm-6 col-xs-12">
-                                    <label for="cm-irudui-irudui">Email</label>
-                                    <input v-model="form_data.email" required class="form-control" name="cm-irudui-irudui" type="email" placeholder="Email" id="newsletter_email">
-                                </div>
+                            <div class="col-sm-6 col-xs-12">
+                                <label for="cm-irudui-irudui">Email</label>
+                                <input v-model="form_data.email" required class="form-control" name="cm-irudui-irudui" type="email" placeholder="Email" id="newsletter_email">
                             </div>
                             <div class="form-group account-btn text-left m-t-10">
                                 <div class="col-xs-12">
